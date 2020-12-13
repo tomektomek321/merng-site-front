7cb38999d6379b29200ea0041c17695d6f82c979
Tomek <tomek1989gw@wppl>
2020-12-13 21:34:30 +0100

    basic semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import 'semantic-ui-css/semantic.min.css'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
 
+
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Container>
+      <Router>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Router>
+    </Container>
   );
 }
 
