87943b2b997506cb09ad72642dc718fde53044b2
Tomek <tomek1989gw@wppl>
2020-12-16 02:19:41 +0100

    display post

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -6,6 +6,8 @@ import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 
+import DeleteButton from './DeleteButton';
+
 export default function PostCard(props) {
     const { user } = useContext(AuthContext);
 
@@ -45,18 +47,7 @@ export default function PostCard(props) {
                         {commentCount}
                     </Label>
                 </Button>
-
-                {user && user.username === username && (
-                    <Button
-                        as="div"
-                        color="red"
-                        floated="right"
-                        onClick={() => console.log('Delete post')}>
-
-                        <Icon name="trash" style={{ margin: 0 }} />
-
-                    </Button>
-                )}
+                {user && user.username === username && <DeleteButton postId={id} />}
 
             </Card.Content>
     </Card>
